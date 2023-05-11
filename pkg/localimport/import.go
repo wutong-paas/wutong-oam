f236bb536995e5f2879831b3522a48f8cb203f94
poneding <poneding@gmail.com>
2023-05-11 15:47:07 +0800

    remove image trans on import

--- pkg/localimport/import.go
+++ pkg/localimport/import.go
@@ -32,7 +32,6 @@ import (
 	"github.com/wutong-paas/wutong-oam/pkg/export"
 	"github.com/wutong-paas/wutong-oam/pkg/ram/v1alpha1"
 	"github.com/wutong-paas/wutong-oam/pkg/util"
-	"github.com/wutong-paas/wutong-oam/pkg/util/docker"
 	"github.com/wutong-paas/wutong-oam/pkg/util/image"
 )
 
@@ -86,16 +85,16 @@ func (r *ramImport) Import(filePath string, hubInfo v1alpha1.ImageInfo) (*v1alph
 	// read app meta config
 	files, _ := ioutil.ReadDir(r.homeDir)
 	if len(files) < 1 {
-		return nil, fmt.Errorf("Failed to read files in tmp dir %s", r.homeDir)
+		return nil, fmt.Errorf("failed to read files in tmp dir %s", r.homeDir)
 	}
 	metaFile, err := os.Open(path.Join(r.homeDir, files[0].Name(), "metadata.json"))
 	if err != nil {
-		return nil, fmt.Errorf("Failed to read files in tmp dir %s: %v", r.homeDir, err)
+		return nil, fmt.Errorf("failed to read files in tmp dir %s: %v", r.homeDir, err)
 	}
 	defer metaFile.Close()
 	var ram v1alpha1.WutongApplicationConfig
 	if err := json.NewDecoder(metaFile).Decode(&ram); err != nil {
-		return nil, fmt.Errorf("Failed to read meta file : %v", err)
+		return nil, fmt.Errorf("failed to read meta file : %v", err)
 	}
 	// load all component images and plugin images
 	//after v5.3 package
@@ -126,70 +125,70 @@ func (r *ramImport) Import(filePath string, hubInfo v1alpha1.ImageInfo) (*v1alph
 			com.ShareImage = com.Image
 		}
 		// new hub info
-		newImageName, err := docker.NewImageName(com.ShareImage, hubInfo)
-		if err != nil {
-			r.logger.Errorf("parse image failure %s", err.Error())
-			return nil, err
-		}
-		err = r.imageClient.ImageTag(com.ShareImage, newImageName, 2)
-		if err != nil {
-			//Compatibility History Version
-			if strings.Contains(err.Error(), "No such image") {
-				var saveImage string
-				saveImage, err = docker.GetOldSaveImageName(com.ShareImage, false)
-				if err != nil {
-					return nil, err
-				}
-				err = r.imageClient.ImageTag(saveImage, newImageName, 2)
-			}
-			if err != nil {
-				logrus.Errorf("change image %s tag to %s failure %s", com.ShareImage, newImageName, err.Error())
-				return nil, err
-			}
-		}
-		r.logger.Infof("start push image %s", newImageName)
-		if err := r.imageClient.ImagePush(newImageName, hubInfo.HubUser, hubInfo.HubPassword, 20); err != nil {
-			logrus.Errorf("push image %s failure %s", newImageName, err.Error())
-			return nil, err
-		}
-		r.logger.Infof("push image %s success", newImageName)
-		com.AppImage = hubInfo
-		com.ShareImage = newImageName
+		// newImageName, err := docker.NewImageName(com.ShareImage, hubInfo)
+		// if err != nil {
+		// 	r.logger.Errorf("parse image failure %s", err.Error())
+		// 	return nil, err
+		// }
+		// err = r.imageClient.ImageTag(com.ShareImage, newImageName, 2)
+		// if err != nil {
+		// 	//Compatibility History Version
+		// 	if strings.Contains(err.Error(), "No such image") {
+		// 		var saveImage string
+		// 		saveImage, err = docker.GetOldSaveImageName(com.ShareImage, false)
+		// 		if err != nil {
+		// 			return nil, err
+		// 		}
+		// 		err = r.imageClient.ImageTag(saveImage, newImageName, 2)
+		// 	}
+		// 	if err != nil {
+		// 		logrus.Errorf("change image %s tag to %s failure %s", com.ShareImage, newImageName, err.Error())
+		// 		return nil, err
+		// 	}
+		// }
+		// r.logger.Infof("start push image %s", newImageName)
+		// if err := r.imageClient.ImagePush(newImageName, hubInfo.HubUser, hubInfo.HubPassword, 20); err != nil {
+		// 	logrus.Errorf("push image %s failure %s", newImageName, err.Error())
+		// 	return nil, err
+		// }
+		// r.logger.Infof("push image %s success", newImageName)
+		// com.AppImage = hubInfo
+		// com.ShareImage = newImageName
 	}
-	for i, plugin := range ram.Plugins {
+	for _, plugin := range ram.Plugins {
 		if plugin.ShareImage == "" {
 			plugin.ShareImage = plugin.Image
 		}
 		// new hub info
-		newImageName, err := docker.NewImageName(plugin.ShareImage, hubInfo)
-		if err != nil {
-			r.logger.Errorf("parse image failure %s", err.Error())
-			return nil, err
-		}
-		err = r.imageClient.ImageTag(plugin.ShareImage, newImageName, 2)
-		if err != nil {
-			//Compatibility History Version
-			if strings.Contains(err.Error(), "No such image") {
-				var saveImage string
-				saveImage, err = docker.GetOldSaveImageName(plugin.ShareImage, false)
-				if err != nil {
-					return nil, err
-				}
-				err = r.imageClient.ImageTag(saveImage, newImageName, 2)
-			}
-			if err != nil {
-				logrus.Errorf("change image %s tag to %s failure %s", plugin.ShareImage, newImageName, err.Error())
-				return nil, err
-			}
-		}
-		r.logger.Infof("start push image %s", newImageName)
-		if err := r.imageClient.ImagePush(newImageName, hubInfo.HubUser, hubInfo.HubPassword, 20); err != nil {
-			logrus.Errorf("push image %s failure %s", newImageName, err.Error())
-			return nil, err
-		}
-		r.logger.Infof("push image %s success", newImageName)
-		ram.Plugins[i].PluginImage = hubInfo
-		ram.Plugins[i].ShareImage = newImageName
+		// newImageName, err := docker.NewImageName(plugin.ShareImage, hubInfo)
+		// if err != nil {
+		// 	r.logger.Errorf("parse image failure %s", err.Error())
+		// 	return nil, err
+		// }
+		// err = r.imageClient.ImageTag(plugin.ShareImage, newImageName, 2)
+		// if err != nil {
+		// 	//Compatibility History Version
+		// 	if strings.Contains(err.Error(), "No such image") {
+		// 		var saveImage string
+		// 		saveImage, err = docker.GetOldSaveImageName(plugin.ShareImage, false)
+		// 		if err != nil {
+		// 			return nil, err
+		// 		}
+		// 		err = r.imageClient.ImageTag(saveImage, newImageName, 2)
+		// 	}
+		// 	if err != nil {
+		// 		logrus.Errorf("change image %s tag to %s failure %s", plugin.ShareImage, newImageName, err.Error())
+		// 		return nil, err
+		// 	}
+		// }
+		// r.logger.Infof("start push image %s", newImageName)
+		// if err := r.imageClient.ImagePush(newImageName, hubInfo.HubUser, hubInfo.HubPassword, 20); err != nil {
+		// 	logrus.Errorf("push image %s failure %s", newImageName, err.Error())
+		// 	return nil, err
+		// }
+		// r.logger.Infof("push image %s success", newImageName)
+		// ram.Plugins[i].PluginImage = hubInfo
+		// ram.Plugins[i].ShareImage = newImageName
 	}
 	return &ram, nil
 }
